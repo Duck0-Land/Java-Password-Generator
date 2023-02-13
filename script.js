4efd4809ad70260afd25be076c51e53d7edf3966
Duck0-Land <erhilston@gmail.com>
2023-02-13 18:43:18 -0500

    function modification

--- script.js
+++ script.js
@@ -54,30 +54,6 @@ function generatePassword() {
     return "Please select at least one character type to use.";
   };
 
-  // group selected characters
-  if (hasLowercase) {
-    possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
-  }
-  if (hasUppercase) {
-    possibleCharacters = possibleCharacters.concat(uppercaseCharacters);
-  }
-  if (hasNumbers) {
-    possibleCharacters = possibleCharacters.concat(numericCharacters);
-  }
-  if (hasSpecial) {
-    possibleCharacters = possibleCharacters.concat(specialCharacters);
-  }
-
-  // pick random cards out of new pool for length of password
-  let finalPassword = ""
-  for (let i = 0; i < numberOfCharacters; i++) {
-    let rng =[Math.floor(Math.random() * possibleCharacters.length)];
-    // or finalPassword += possibleCharacters[rng];
-    finalPassword = finalPassword + possibleCharacters[rng];
-  }
-  return finalPassword;
-};
-
 
 
 //starter
